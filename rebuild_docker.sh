f1c9997ce36499eb67a7a916d22473d5552bd034
Andrew Kicklighter <apkicklighter@gmail.com>
2023-04-02 01:51:58 -0500

    add comments to shell

--- rebuild_docker.sh
+++ rebuild_docker.sh
@@ -1,4 +1,16 @@
+echo STOPPING PONG BOT..
 docker stop FCFB-Pong-Bot
+echo PONG BOT STOPPED!
+echo
+echo REMOVING OLD PONG BOT...
 docker remove FCFB-Pong-Bot
+echo OLD PONG BOT REMOVED!
+echo
+echo BUILDING NEW PONG BOT...
 docker build -t "fcfb-pong-bot:Dockerfile" .
+echo NEW PONG BOT BUILT!
+echo
+echo STARTING NEW PONG BOT...
 docker run -d --restart=always --name FCFB-Pong-Bot fcfb-pong-bot:Dockerfile
+echo NEW PONG BOT STARTED!
+echo DONE!

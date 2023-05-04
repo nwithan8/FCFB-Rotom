33c818a8520377a45687b7389071d6f0bad9adc4
Andrew Kicklighter <apkicklighter@gmail.com>
2023-05-03 21:58:18 -0500

    add fcs

--- pong/discord/discord_functions.py
+++ pong/discord/discord_functions.py
@@ -27,23 +27,22 @@ async def ping_user(client, user, message_content):
         return False
     else:
         print("User is signed up for pings, ping them")
-        server_name = client.guild.name
-        print(server_name)
-        if server_name == "Fake College Football":
-            channel = client.get_channel(int(config_data['fbs_channel_id']))
-            try:
-                user = await client.fetch_user(int(discord_id[0]))
-            except:
-                return False
+        for guild in client.guilds:
+            server_name = guild.name
+            if server_name == "Fake College Football":
+                channel = client.get_channel(int(config_data['fbs_channel_id']))
+                try:
+                    user = await client.fetch_user(int(discord_id[0]))
+                except:
+                    continue
 
-            await channel.send(f"{user.mention} {message_content}")
-            return True
-        elif server_name == "Fake FCS":
-            channel = client.get_channel(int(config_data['fcs_channel_id']))
-            try:
-                user = await client.fetch_user(int(discord_id[0]))
-            except:
-                return False
+                await channel.send(f"{user.mention} {message_content}")
+            elif server_name == "Fake FCS":
+                channel = client.get_channel(int(config_data['fcs_channel_id']))
+                try:
+                    user = await client.fetch_user(int(discord_id[0]))
+                except:
+                    continue
 
-            await channel.send(f"{user.mention} {message_content}")
-            return True
+                await channel.send(f"{user.mention} {message_content}")
+        return True

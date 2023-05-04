faf1912a08509bf4521b7ecc8264f6e6d4934c99
Andrew Kicklighter <apkicklighter@gmail.com>
2023-05-04 14:54:34 -0500

    handle users without choice

--- pong/discord/discord_functions.py
+++ pong/discord/discord_functions.py
@@ -29,7 +29,7 @@ async def ping_user(client, user, message_content):
     else:
         print("User is signed up for pings, ping them")
         server_to_ping = await get_player_server_to_ping(user)
-        if not server_to_ping:
+        if server_to_ping[0] is None:
             server_to_ping = 0
         else:
             server_to_ping = server_to_ping[0]
